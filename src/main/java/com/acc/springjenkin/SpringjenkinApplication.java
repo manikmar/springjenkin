e00dd55b2db50cfa4c075f3159347bed9a6a92eb
Mani <manikumar.panithi@gmail.com>
2023-01-02 14:35:44 +0530

    first commit

---
+++ src/main/java/com/acc/springjenkin/SpringjenkinApplication.java
@@ -0,0 +1,13 @@
+package com.acc.springjenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjenkinApplication.class, args);
+	}
+
+}
